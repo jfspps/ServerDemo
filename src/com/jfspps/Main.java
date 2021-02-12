a8c714da199e0ad952c03f8d74e8697e09afb91e
jfspps <jfsapps@gmail.com>
2021-02-12 21:43:14 +0000

    enabled multiple clients (with a blocking limitation)

--- src/com/jfspps/Main.java
+++ src/com/jfspps/Main.java
@@ -14,19 +14,25 @@ public class Main {
         // In Java, ServerSockets wait for requests and Sockets generate requests
         try (ServerSocket serverSocket = new ServerSocket(5000)){
 
-            // Socket is used by this server to connect to a client and establish I/O streams
-            Socket socket = serverSocket.accept();
-            System.out.println("Client connected");
-
-            BufferedReader input = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream())
-            );
-
-            // true to flush data automatically and ensure data is sent
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-
+            // enable more client connections
             while(true){
+                // Socket is used by this server to connect to a client and establish I/O streams
+                Socket socket = serverSocket.accept();
+                System.out.println("Client connected");
+
+                BufferedReader input = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream())
+                );
+
+                // true to flush data automatically and ensure data is sent
+                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+                // this is blocking
+                // the first client must enter message; immediately, the server is open to a second client
+                // the first client cannot enter further messages until a connection to the second server is established
+                // this requirement perpetuates for subsequent connections and "echoes"
                 String echoString = input.readLine();
+
                 if (echoString.equals("exit")){
                     break;
                 }
